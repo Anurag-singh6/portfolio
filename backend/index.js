b187ec054d07c9aa89c5413bed50ab7ad39a6d12
Anurag-singh6 <anuragsingh76000@gmail.com>
2026-03-25 17:51:09 +0530

    index up

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).json({ message: errorMessage });
 });
 
-const port = process.env.PORT || 4500;
+const port = process.env.FRONTEND_URL || 4500;
 app.listen(port, async () => {
   console.log("server started at port ", port);
   await connectdb();
