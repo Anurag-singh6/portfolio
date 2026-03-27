5cf15dd515de009f7a4b80d9530bc86da3e508ab
Anurag-singh6 <anuragsingh76000@gmail.com>
2026-03-27 14:27:37 +0530

    get

--- backend/src/routers/publicRouter.js
+++ backend/src/routers/publicRouter.js
@@ -2,6 +2,6 @@ import express from "express";
 import { UserContact } from "../controllers/usercontact.js";
 
 const router = express.Router();
-router.post("/contact", UserContact);
+router.get("/contact", UserContact);
 
 export default router;
